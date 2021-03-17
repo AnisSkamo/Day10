817efc6aacf4e8b49b4b8828eb569b1a95532e4e
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-17 20:00:58 +0100

    Add files via upload

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,13 @@
-
+// Only change code below this line
+var myPet = {
+     species: "Happy",
+     name: "Rex",
+     legs: 4,
+     friends: ["Lesi", "Tim"]
+}
+function myFunction(obj) {
+    return obj;
+}
+// Only change code above this line
+console.log(myFunction(myPet)); 
+module.exports = { myPet, myFunction};
