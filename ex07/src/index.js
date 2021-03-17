b5e809d17b34bf04788e71940539eb8e4be6ab66
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-17 22:05:10 +0100

    Add files via upload

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,1 +1,13 @@
+function myFunction(propName) { // Change this line
 
+var lion = {
+    "name": "Simba",
+    "legs": 4,
+    "tails": 1,
+    "roar": "roar-roar"
+};
+}
+delete lion ["roar"];
+return lion  
+console.log(myFunction("roar")); // Change this line
+module.exports = myFunction;
