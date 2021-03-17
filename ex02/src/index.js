d29678373013d2d20fcd3a624ff06e01e2ceaf2c
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-17 20:15:50 +0100

    Add files via upload

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,15 @@
-
+// Only change code below this line
+var myClothes = {
+    hat: "Fedora",
+    shirt: "Nike",
+    shoes: "Converse"
+};
+function myFunction(myObj) {
+    var hatValue = myClothes.hat; // value1
+    var shirtValue = myClothes.shirt; // value2
+    var shoesValue = myClothes.shoes; // value2
+    return { hatValue, shirtValue, shoesValue } ;
+}
+// Onlz change code above this line
+console.log(myFunction(myClothes)); 
+module.exports = myFunction(myClothes);
