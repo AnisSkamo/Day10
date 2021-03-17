38ea27f48cf4e93a97acd15a307e3f1e289b47ea
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-17 21:37:11 +0100

    Add files via upload

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,16 @@
-
+// Create lion object here
+var lion = {
+    "name": "Simba",
+    "legs": 4,
+    "tails": 1,
+};
+// End od lion object
+function myFunction(lionName, lionValue) { // Change this line
+// Only change code below this line
+lion["roar"] = "roar-roar"
+lion[lionName] = lionValue;
+return lion;
+// Only change code above this line
+}
+console.log(myFunction("roar", "roar-roar")); // Change this line
+module.exports = myFunction;
