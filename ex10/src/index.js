56be5d015e1dea4d26c9a63de9361f7eac3e681b
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-18 09:41:49 +0100

    Add files via upload

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,1 +1,32 @@
-
+function myFunction() {
+    var myMusic = {
+        001: {
+            artist: "Billy Joel",
+            title: "Piano Man",
+            release_year: 1973,
+            formats: {
+                1: "CD",
+                2: "8T",
+                3: "LP"
+            },
+            gold: true
+        },
+        002: {
+            artist: "Whitney Houston",
+            title: "All at Once",
+            release_year: 1985,
+            formats: {
+                1: "CD",
+                2: "8T",
+                3: "LP"
+            },
+            gold: true
+        // Add a record here
+        }
+       
+    };
+   return myMusic
+}
+console.log(myFunction()[2]);
+myFunction()[2];
+module.exports = myFunction;
